9c17154d83a3741623a2e8915c30f0f7dc85f57a
Alireza Salehi <engalirezasalehi@gmail.com>
2025-01-07 04:41:48 -0800

    Add Route Management to Micro source code

--- sketch_jan1a/sketch_jan1a.ino
+++ sketch_jan1a/sketch_jan1a.ino
@@ -1,67 +1,79 @@
-#include <ESP8266WiFi.h>  
-#include <ESPAsyncWebServer.h>  
-#include <SD.h>  
-#include <SPI.h>  
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <SD.h>
 
-const char* ssid = "MogileAp";         // Replace with your network SSID  
-const char* password = "Aa1364123110"; // Replace with your network password  
+const char* ssid = "MobileAP";          // Replace with your Wi-Fi SSID
+const char* password = "Aa1364123110";  // Replace with your Wi-Fi password
 
-AsyncWebServer server(80);  
-const int chipSelect = D2; // CS pin for SD card  
+enum FILE_TYPE {
+  HTML = 1,
+  JS
+} ;
+
+ESP8266WebServer server(80);
+
 void setup() {
-    Serial.begin(115200);  
-  
-  // Set up the built-in LED pin  
-  pinMode(LED_BUILTIN, OUTPUT);  
-  digitalWrite(LED_BUILTIN, HIGH); // Turn the LED off initially  
+  Serial.begin(115200);
 
-  // Connect to Wi-Fi  
-  WiFi.begin(ssid, password);  
-  while (WiFi.status() != WL_CONNECTED) {  
-    delay(1000);  
-    Serial.println("Connecting to WiFi...");  
-  }  
-  Serial.println("Connected to WiFi");  
-  Serial.println(WiFi.localIP()); // Print the IP address  
+  // Initialize SD card
+  if (!SD.begin(SS)) {
+    Serial.println("SD card initialization failed!");
+    return;
+  }
+  Serial.println("SD card initialized.");
 
-  // Initialize the SD card  
-  if (!SD.begin(chipSelect)) {  
-    Serial.println("SD Card initialization failed!");  
-    return; // Stop if SD card initialization fails  
-  }  
-  Serial.println("SD Card initialized successfully");  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+  Serial.println(WiFi.localIP());
 
-  // Define routes  
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){  
-    File file = SD.open("/index.html");  
-    if (file) {  
-      request->send(file, "text/html");  
-      file.close();  
-    } else {  
-      request->send(404, "text/plain", "File not found");  
-    }  
-  });  
+  // Serve HTML file
+  //server.on("/", handleRoot);
+  ManageRoutes("index.html", FILE_TYPE::HTML);
 
-  server.on("/led/on", HTTP_GET, [](AsyncWebServerRequest *request){  
-    digitalWrite(LED_BUILTIN, LOW); // Turn LED on  
-    request->send(200, "text/plain", "LED is ON");  
-  });  
-
-  server.on("/led/off", HTTP_GET, [](AsyncWebServerRequest *request){  
-    digitalWrite(LED_BUILTIN, HIGH); // Turn LED off  
-    request->send(200, "text/plain", "LED is OFF");  
-  });  
-
-  server.onNotFound([](AsyncWebServerRequest *request){  
-    request->send(404, "text/plain", "404: Not Found");  
-  });  
-
-  // Start the server  
-  server.begin();  
-
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  server.handleClient();
 }
+
+void ManageRoutes(String fileName, FILE_TYPE type){
+  //Serial.println("/"+fileName);
+  switch(type){
+    case FILE_TYPE::HTML : 
+       server.on("/" + fileName,[ fileName ](){
+            Serial.println(fileName);
+            File file = SD.open("/" + fileName);  // Replace with your HTML file name
+            if (file) {
+              server.streamFile(file, "text/html");
+              file.close();
+            } else {
+              server.send(200, "text/plain", "Failed to open HTML file");
+            }        
+       });
+       break;
+    case FILE_TYPE::JS :   
+       server.on("/" + fileName,[ fileName](){
+            Serial.println(fileName);
+            File file = SD.open("/" + fileName);  // Replace with your HTML file name
+            if (file) {
+              server.streamFile(file, "text/html");
+              file.close();
+            } else {
+              server.send(200, "text/plain", "Failed to open HTML file");
+            }        
+       });
+       break;  
+    default : 
+       break;        
+  }
+}
+
+
