a108e89f99aeee1a219da3ec42cc93cc0c1b1ef2
Alireza Salehi <engsalehi@live.com>
2025-01-07 23:33:37 +0330

    Add javascript web method wrapper

--- js/sensors.js
+++ js/sensors.js
@@ -1,5 +1,17 @@
 
 // Tag id : A2D 
-function readA2D(){
-    
+async function readA2D(){
+    const startDateTime = Date.now();
+
+    setTimeout(() => {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if(this.readyState === 4 && this.status === 200){
+                document.getElementById("analog_data").innerText = this.responseText;
+            }
+        };
+        xhttp.open("GET","/a2d", true);
+        xhttp.send():
+    }, 2000);
+
 }
