bd16a860316d325a38bfebe5e26d7fa68452fc64
Alireza Salehi <engalirezasalehi@gmail.com>
2025-01-12 05:36:00 -0800

    Add IP Configuration HTML Panel

--- js/settings.js
+++ js/settings.js
@@ -8,3 +8,33 @@ function loadSetting(){
     xhttp.open("GET", "ajax_info.txt");
     xhttp.send();
 }
+
+async function rbChangeWIFIMode() {
+   
+    var ipAddress = document.getElementById("ipAddress");
+    var gateway = document.getElementById("gateway");
+    var subnetMask = document.getElementById("subnetMask");
+    var primaryDNS = document.getElementById("primaryDNS");
+    var secondaryDNS = document.getElementById("secondaryDNS");
+
+    const wifiMode = document.getElementById("rbHotspot");
+    //console.log(wifiMode.attributes);
+
+    if(wifiMode.checked){
+        ipAddress.setAttribute("disabled", "");
+        gateway.setAttribute("disabled", "");
+        subnetMask.setAttribute("disabled", "");
+        primaryDNS.setAttribute("disabled", "");
+        secondaryDNS.setAttribute("disabled", "");
+  
+    }else{
+        ipAddress.removeAttribute("disabled");
+        gateway.removeAttribute("disabled");
+        subnetMask.removeAttribute("disabled");
+        primaryDNS.removeAttribute("disabled");
+        secondaryDNS.removeAttribute("disabled");
+
+      
+
+    }
+}
