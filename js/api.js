c67bd830bcca651f635744b3ab96aefe958a80fa
Alireza Salehi <engalirezasalehi@gmail.com>
2025-01-18 17:57:18 +0330

    Add .gitIgnore file

--- js/api.js
+++ js/api.js
@@ -1,35 +1,33 @@
-// in this file we should collect all information and function 
+// in this file we should collect all information and function
 // for gathering required data and send to ESP8266 Microcontroller
 
- async function sendNetworkSetting(netJson, lang){
-    $.ajax({                    
-        url: "/change-net_config",
-        type: "POST",
-        data: netJson,
-        contentType: 'application/json',
-        success: function (data) {
-            //alert(CS(lang).network_save_cfg_success);
-            return true;
-        },
-        error: function (xhRequest, ErrorText, thrownError) {
-            alert(ErrorText);
-            return false;
-        }
-    });
-
+async function sendNetworkSetting(netJson, lang) {
+  $.ajax({
+    url: "/change-net_config",
+    type: "POST",
+    data: netJson,
+    contentType: "application/json",
+    success: function (data) {
+      //alert(CS(lang).network_save_cfg_success);
+      return true;
+    },
+    error: function (xhRequest, ErrorText, thrownError) {
+      alert(ErrorText);
+      return false;
+    },
+  });
 }
 
-async function getConfig(section){
-    $.ajax({  
-        url: '/get-config?section='+section, // Replace with your URL  
-        type: 'GET',  
-        dataType: 'json', // Expect a JSON response  
-        success: function(response) {  
-            console.log('Success:', response);  
-        },  
-        error: function(xhr, status, error) {  
-            console.error('Error:', error);  
-        }  
-    }); 
-
+async function getConfig(section) {
+  $.ajax({
+    url: "/get-config", // Replace with your URL
+    type: "GET",
+    dataType: "json", // Expect a JSON response
+    success: function (response) {
+        return response;
+    },
+    error: function (xhr, status, error) {
+      console.error("Error:", error);
+    },
+  });
 }
--- js/ui-evt.js
+++ js/ui-evt.js
@@ -1,52 +1,55 @@
-
-
 const lang = document.body.lang;
 
-
 $(document).ready(function () {
-   readA2D();
-   readConnectionStatus();
-   readHardwareInfo();
-   readSDInfo();
-   getConfig('network');
- });
-
-$('#btnSaveNetSetting').click(async function (e) {
-
-   const netConfig = makeWifiConfigJSON();
-   const isIpValid = isValidIP(netConfig.ip);
-   const isValidPrimaryDNS = isValidIP(netConfig.pd);
-   const isValidGateway = isValidIP(netConfig.gw);
-   const isValidSubnetMask = isValidIP(netConfig.sm);
-   const isValidSecondaryDNS = isValidIP(netConfig.sd);
-
-
-   const isValidSSID = isValidSSIDName(netConfig.wn);
-
-   if (!isIpValid || !isValidGateway || !isValidPrimaryDNS || !isValidSubnetMask || !isValidSecondaryDNS) {
-      $("#error_panel").css({'display': 'block'});
-      $('#error_panel').text(CS(lang).network_setting_error);
-
-   } else {
-      $("#error_panel").css({'display': 'none'});
-      sendNetworkSetting(netConfig, lang).then(r => {document.getElementById("btnCancelNetSetting").click();}).catch(error => {});
-
-   }
+  readA2D();
+  readConnectionStatus();
+  readHardwareInfo();
+  readSDInfo();
+  getConfig('network');
 });
 
-$('#ipAddress').on("keypress", function(evt){
-   //console.log(evt);
-   if ((evt.key < '0' || evt.key > '9') && evt.key !== '.') {
-       event.preventDefault()
-     }
+$("#btnSaveNetSetting").click(async function (e) {
+  const netConfig = makeWifiConfigJSON();
+  const isIpValid = isValidIP(netConfig.ip);
+  const isValidPrimaryDNS = isValidIP(netConfig.pd);
+  const isValidGateway = isValidIP(netConfig.gw);
+  const isValidSubnetMask = isValidIP(netConfig.sm);
+  const isValidSecondaryDNS = isValidIP(netConfig.sd);
+
+  const isValidSSID = isValidSSIDName(netConfig.wn);
+
+  if (
+    !isIpValid ||
+    !isValidGateway ||
+    !isValidPrimaryDNS ||
+    !isValidSubnetMask ||
+    !isValidSecondaryDNS
+  ) {
+    $("#error_panel").css({ display: "block" });
+    $("#error_panel").text(CS(lang).network_setting_error);
+  } else {
+    $("#error_panel").css({ display: "none" });
+    sendNetworkSetting(netConfig, lang)
+      .then((r) => {
+        document.getElementById("btnCancelNetSetting").click();
+      })
+      .catch((error) => {});
+  }
 });
 
+$("#ipAddress").on("keypress", function (evt) {
+  //console.log(evt);
+  if ((evt.key < "0" || evt.key > "9") && evt.key !== ".") {
+    event.preventDefault();
+  }
+});
 
-function isValidIP(ipaddress) {  
-   return /^(25[0-5]|2[0-4][0-9]|[1]?[1-9][1-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[1-9][1-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[1-9][1-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[1-9][1-9]?)$/.test(ipaddress);
+function isValidIP(ipaddress) {
+  return /^(25[0-5]|2[0-4][0-9]|[1]?[1-9][1-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[1-9][1-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[1-9][1-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[1-9][1-9]?)$/.test(
+    ipaddress
+  );
+}
 
-} 
-
-function isValidSSIDName(ssidName){
-   return /^[!#;].|[+\[\]/"\t\s].*$/.test(ssidName);
+function isValidSSIDName(ssidName) {
+  return /^[!#;].|[+\[\]/"\t\s].*$/.test(ssidName);
 }
